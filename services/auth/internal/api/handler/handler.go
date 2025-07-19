f61dbf7f73c3125cbf97d8263b7ec9caf845f261
m16w <m16w.eth@gmail.com>
2025-07-19 13:12:18 -0700

    add rate limits

--- services/auth/internal/api/handler/handler.go
+++ services/auth/internal/api/handler/handler.go
@@ -1,6 +1,8 @@
 package handler
 
 import (
+	"time"
+
 	commonCache "github.com/spazzle-io/spazzle-api/libs/common/cache"
 	pb "github.com/spazzle-io/spazzle-api/services/proto/auth/auth/v1"
 
@@ -29,5 +31,11 @@ func New(config util.Config, store db.Store, cache commonCache.Cache, tokenMaker
 }
 
 func (h *Handler) RateLimits() map[string]commonMiddleware.Rate {
-	return map[string]commonMiddleware.Rate{}
+	return map[string]commonMiddleware.Rate{
+		"/auth.v1.AuthService/GetSIWEPayload":      {Aliases: []string{"GET:/auth/siwe-payload"}, Limit: 10, Period: time.Minute, Identifier: "GetSIWEPayload"},
+		"/auth.v1.AuthService/Authenticate":        {Aliases: []string{"POST:/auth/authenticate"}, Limit: 10, Period: time.Minute, Identifier: "Authenticate"},
+		"/auth.v1.AuthService/VerifyAccessToken":   {Aliases: []string{"POST:/auth/verify-access-token"}, Limit: 120, Period: time.Minute, Identifier: "VerifyAccessToken"},
+		"/auth.v1.AuthService/RefreshAccessToken":  {Aliases: []string{"POST:/auth/refresh-access-token"}, Limit: 10, Period: time.Minute, Identifier: "RefreshAccessToken"},
+		"/auth.v1.AuthService/RevokeRefreshTokens": {Aliases: []string{"POST:/auth/revoke-refresh-tokens"}, Limit: 10, Period: time.Minute, Identifier: "RevokeRefreshTokens"},
+	}
 }
