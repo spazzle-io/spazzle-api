b4be3ce2dcee8678eabc3a99d5347d5dc0a8267f
m16w.eth <154888072+m16w@users.noreply.github.com>
2025-07-20 17:11:38 -0700

    feat(users): add create user db tx (#28)

--- services/users/internal/db/sqlc/store.go
+++ services/users/internal/db/sqlc/store.go
@@ -1,11 +1,15 @@
 package db
 
 import (
+	"context"
+	"fmt"
+
 	"github.com/jackc/pgx/v5/pgxpool"
 )
 
 type Store interface {
 	Querier
+	CreateUserTx(ctx context.Context, params CreateUserTxParams) (CreateUserTxResult, error)
 }
 
 type SQLStore struct {
@@ -18,4 +22,23 @@ func NewStore(connPool *pgxpool.Pool) Store {
 		connPool: connPool,
 		Queries:  New(connPool),
 	}
+}
+
+func (store *SQLStore) execTx(ctx context.Context, fn func(*Queries) error) error {
+	tx, err := store.connPool.Begin(ctx)
+	if err != nil {
+		return err
+	}
+
+	q := New(tx)
+	err = fn(q)
+	if err != nil {
+		if rbErr := tx.Rollback(ctx); rbErr != nil {
+			return fmt.Errorf("transaction err: %v, rollback err: %v", err, rbErr)
+		}
+
+		return err
+	}
+
+	return tx.Commit(ctx)
 }
