3eecf139ab9772c20b4c2c97a21ba3e52a3d35e6
m16w.eth <154888072+m16w@users.noreply.github.com>
2025-08-03 20:50:14 -0700

    feat(users): select required columns sql (#37)

--- services/users/internal/db/query/user.sql
+++ services/users/internal/db/query/user.sql
@@ -6,7 +6,9 @@ INSERT INTO users (
 ) RETURNING *;
 
 -- name: GetUserById :one
-SELECT * FROM users
+SELECT
+    id, wallet_address, gamer_tag, created_at
+FROM users
 WHERE id = sqlc.arg(user_id)
 LIMIT 1;
 
@@ -14,7 +16,9 @@ LIMIT 1;
 SELECT COUNT(*) as total_users FROM users;
 
 -- name: ListUsers :many
-SELECT * FROM users
+SELECT
+    id, wallet_address, gamer_tag, created_at
+FROM users
 ORDER BY created_at DESC
 LIMIT $1
 OFFSET $2;
@@ -22,11 +26,7 @@ OFFSET $2;
 -- name: UpdateUser :one
 UPDATE users
 SET
-    gamer_tag = COALESCE(sqlc.narg(gamer_tag), gamer_tag),
-    ens_name = COALESCE(sqlc.narg(ens_name), ens_name),
-    ens_avatar_uri = COALESCE(sqlc.narg(ens_avatar_uri), ens_avatar_uri),
-    ens_image_url = COALESCE(sqlc.narg(ens_image_url), ens_image_url),
-    ens_last_resolved_at = COALESCE(sqlc.narg(ens_last_resolved_at), ens_last_resolved_at)
+    gamer_tag = COALESCE(sqlc.narg(gamer_tag), gamer_tag)
 WHERE
     id = sqlc.arg(user_id)
 RETURNING *;
