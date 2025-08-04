bc9f7fab9672c464a1604b3f651b21b56632575a
m16w.eth <154888072+m16w@users.noreply.github.com>
2025-08-03 20:34:01 -0700

    feat(users): rollback async background task processor (#36)

--- services/users/internal/api/handler/handler.go
+++ services/users/internal/api/handler/handler.go
@@ -5,22 +5,19 @@ import (
 	commonMiddleware "github.com/spazzle-io/spazzle-api/libs/common/middleware"
 	db "github.com/spazzle-io/spazzle-api/services/users/internal/db/sqlc"
 	"github.com/spazzle-io/spazzle-api/services/users/internal/util"
-	"github.com/spazzle-io/spazzle-api/services/users/internal/worker"
 )
 
 type Handler struct {
-	config          util.Config
-	store           db.Store
-	cache           commonCache.Cache
-	taskDistributor worker.TaskDistributor
+	config util.Config
+	store  db.Store
+	cache  commonCache.Cache
 }
 
-func New(config util.Config, store db.Store, cache commonCache.Cache, taskDistributor worker.TaskDistributor) *Handler {
+func New(config util.Config, store db.Store, cache commonCache.Cache) *Handler {
 	return &Handler{
-		config:          config,
-		store:           store,
-		cache:           cache,
-		taskDistributor: taskDistributor,
+		config: config,
+		store:  store,
+		cache:  cache,
 	}
 }
 
